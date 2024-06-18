a86462ea5f85c6b57b641d88352e096baf6328ab
Javan <Javan@Javans-MacBook-Pro.local>
2024-06-18 12:25:23 +0100

    adds featured articles on homepage, adds componeent that fetches all articles

--- src/components/comp_sets/index.jsx
+++ src/components/comp_sets/index.jsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom"
 import logo from "../../assets/logo.png"
+import { getAllArticles } from "../../apis/get-api";
+import { useEffect, useState } from "react";
+import thumbsUp from "../../assets/thumbs+up.svg"
 
 export const NCNHeader = () => {
 
@@ -11,7 +14,7 @@ function handleNavigation (e) {
         <header>
           <img src={logo} alt="Website Logo" />
           <nav aria-label="Main Navigation">
-            <ul>
+            <ul className="NavBar">
               <li>
                 <Link to="/">Home</Link>
               </li>
@@ -28,5 +31,125 @@ function handleNavigation (e) {
     }
 
 
+
+
+export const FetchFeaturedArticles = () => {
+
+    const [fetchedArticles, setFetchedArticles] = useState([])
+
+    useEffect(() => {
+        getAllArticles().then(({article}) => {
+        setFetchedArticles(article)
+    
+    })
+}, [])
+const featuredArticles = fetchedArticles.splice(0, 5)
+
+return (
+
+    <ul className="featured">
+
+        {featuredArticles.map((article, i) => {
+            
+            return (
+                <li key={i}>
+            <article>
+              <img
+                className="profilePic"
+                src="https://wallpapers.com/images/featured/cool-profile-picture-87h46gcobjl5e4xu.jpg"
+              ></img>
+              <div className="articlePreview">
+                <h2 className="headline">{article.title}</h2>
+                <p className="body">
+                  Lorem ipsum dolor sit amet consectetur. Natoque non sed urna
+                  habitasse ultrices. Arcu blandit condimentum ultricies risus sit.
+                  Risus eu non at massa scelerisque sapien leo. Auctor ornare morbi
+                  sed urna morbi porttitor.
+                  Lorem ipsum dolor sit amet consectetur. Natoque non sed urna
+                 {article.body}
+            
+                  Lorem ipsum dolor sit amet consectetur. Natoque non sed urna
+                  habitasse ultrices. Arcu blandit condimentum ultricies risus sit.
+                  Risus eu non at massa scelerisque sapien leo. Auctor ornare morbi
+                  sed urna morbi porttitor.
+                </p>
+                <p className="author">by {article.author}</p>
+              </div>
+            </article>
+            <div className="likeFrame">
+              <img src={thumbsUp}></img>
+              <h3 className="likeCount">{article.votes}</h3>
+            </div>
+            <h3 className="topicTag">Topic:{article.topic}</h3>
+        </li>
+
+            )
+        })}
+
+    </ul>
+
+)
+
+}
+
+export const FetchAllArticles = () => {
+
+    const [fetchedArticles, setFetchedArticles] = useState([])
+
+    useEffect(() => {
+        getAllArticles().then(({article}) => {
+        setFetchedArticles(article)
+    
+    })
+}, [])
+
+return (
+
+    <ul className="featured">
+
+        {fetchedArticles.map((article, i) => {
+            
+            return (
+                <li key={i}>
+            <article>
+              <img
+                className="profilePic"
+                src="https://wallpapers.com/images/featured/cool-profile-picture-87h46gcobjl5e4xu.jpg"
+              ></img>
+              <div className="articlePreview">
+                <h2 className="headline">{article.title}</h2>
+                <p className="body">
+                  Lorem ipsum dolor sit amet consectetur. Natoque non sed urna
+                  habitasse ultrices. Arcu blandit condimentum ultricies risus sit.
+                  Risus eu non at massa scelerisque sapien leo. Auctor ornare morbi
+                  sed urna morbi porttitor.
+                  Lorem ipsum dolor sit amet consectetur. Natoque non sed urna
+                 {article.body}
+            
+                  Lorem ipsum dolor sit amet consectetur. Natoque non sed urna
+                  habitasse ultrices. Arcu blandit condimentum ultricies risus sit.
+                  Risus eu non at massa scelerisque sapien leo. Auctor ornare morbi
+                  sed urna morbi porttitor.
+                </p>
+                <p className="author">by {article.author}</p>
+              </div>
+            </article>
+            <div className="likeFrame">
+              <img src={thumbsUp}></img>
+              <h3 className="likeCount">{article.votes}</h3>
+            </div>
+            <h3 className="topicTag">Topic:{article.topic}</h3>
+        </li>
+
+            )
+        })}
+
+    </ul>
+
+)
+
+}
+
+ 
 
 
--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,11 +1,27 @@
 import { useState } from "react";
 
-export function Pg_Home (){
-
-    return (
-        <h1>this is the home page</h1>
-
-    )
+import thumbsUp from "../../assets/thumbs+up.svg";
+import { getAllArticles } from "../../apis/get-api";
+import { FetchFeaturedArticles } from "../comp_sets";
 
 
+getAllArticles()
+
+export function Pg_Home() {
+  return (
+    <div className="appHome">
+      <div className="redContainer">
+        <h1>What's New?</h1>
+      </div>
+
+      <main>
+        
+
+        <FetchFeaturedArticles/>
+      </main>
+     
+    
+
+    </div>
+  );
 }
